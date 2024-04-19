a14f7319c278a0cb02a2a1a4ea192805d7bae0b7
Furi5 <fu_li5@163.com>
2024-04-19 15:26:32 +0800

    add att py

--- train_Drug_att.py
+++ train_Drug_att.py
@@ -1,31 +1,32 @@
 """Trains a chemprop model on a dataset."""
-import argparse
+
 from chemprop.train import cross_validate, run_training
 from chemprop.args import TrainArgs, HyperoptArgs
 from chemprop.hyperparameter_optimization import hyperopt
 
-task = 'Organ'
-gpu = 0
+task = 'Drug'
+gpu = 1
+
 
 # --------------- Hyperparameter Optimization-------------#
-# hyperparameter_arguments = [
-#     '--data_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/{task}_train.csv',
-#     '--separate_val_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/{task}_valid.csv',
-#     '--separate_test_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/{task}_test.csv',
-#     '--dataset_type', 'classification',
-#     '--hyperopt_checkpoint_dir', f'checkpoints/{task}_hyperopt',
-#     '--gpu', str(gpu),
-#     '--batch_size', '128',
-#     '--num_iters', '10',
-#     '--epochs', '300',
-#     '--metric', 'auc',
-#     '--aggregation', 'norm',
-#     '--search_parameter_keywords', 'depth', 'ffn_num_layers', 'hidden_size', 'ffn_hidden_size', 'dropout',
-#     '--config_save_path', f'checkpoints/{task}_hyperopt/config.json',
-#     '--log_dir', f'checkpoints/{task}_hyperopt',
-# ]
-# hy_args = HyperoptArgs().parse_args(hyperparameter_arguments)
-# hyperopt(args=hy_args)
+hyperparameter_arguments = [
+    '--data_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/cla/{task}_train.csv',
+    '--separate_val_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/cla/{task}_valid.csv',
+    '--separate_test_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/cla/{task}_test.csv',
+    '--dataset_type', 'classification',
+    '--hyperopt_checkpoint_dir', f'checkpoints/{task}/cla/{task}_hyperopt',
+    '--gpu', str(gpu),
+    '--batch_size', '128',
+    '--num_iters', '3',
+    '--epochs', '30',
+    '--metric', 'auc',
+    '--aggregation', 'norm',
+    '--search_parameter_keywords', 'depth', 'ffn_num_layers', 'hidden_size', 'ffn_hidden_size', 'dropout',
+    '--config_save_path', f'checkpoints/{task}/cla/{task}_hyperopt/config.json',
+    '--log_dir', f'checkpoints/{task}/cla/{task}_hyperopt',
+]
+hy_args = HyperoptArgs().parse_args(hyperparameter_arguments)
+hyperopt(args=hy_args)
 
 # ---------------train-------------#
 parser = argparse.ArgumentParser()
@@ -39,7 +40,7 @@ train_arguments = [
     '--separate_test_path', f'/home/fuli/my_code/git/tox_data/tox_data_v1/{task}/{input_args.file}/{task}_test.csv',
     '--config_path', f'checkpoints/{task}/{task}_hyperopt/config.json',
     '--dataset_type', 'classification',
-    '--save_dir', f'checkpoints/{task}/{task}_{input_args.file}_att_model',
+    '--save_dir', f'checkpoints/att/{task}/{task}_{input_args.file}_model',
     '--epochs', '1000',
     '--gpu', str(input_args.gpu),
     '--save_smiles_splits',
