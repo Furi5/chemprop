96b42b1134586880672159f92d8a6a776d10d21f
Furi5 <fu_li5@163.com>
2024-04-28 20:13:08 +0800

    fix train.py

--- train.py
+++ train.py
@@ -5,17 +5,17 @@ from chemprop.train import chemprop_train
 
 if __name__ == '__main__':
     arguments = [
-    '--data_path', 'data/clintox.csv',
-    '--dataset_type', 'classification',
-    '--save_dir', 'test_checkpoints_cla',
-    '--epochs', '10',
-    '--save_smiles_splits',
-    '--no_cuda',
-]
+        '--data_path', 'data/clintox.csv',
+        '--dataset_type', 'classification',
+        '--save_dir', 'test_checkpoints_cla',
+        '--epochs', '10',
+        '--save_smiles_splits',
+        '--no_cuda',
+    ]
 
     args = chemprop.args.TrainArgs().parse_args(arguments)
-    mean_score, std_score = chemprop.train.cross_validate(args=args, train_func=chemprop.train.run_training)
-
+    mean_score, std_score = chemprop.train.cross_validate(
+        args=args, train_func=chemprop.train.run_training)
 
     # arguments = [
     #     '--test_path', '/dev/null',
